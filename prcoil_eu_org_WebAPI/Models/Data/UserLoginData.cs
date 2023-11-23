0784957f3131eae4b370a092d7a1f3ff31bcd649
Prcoil <69021881+PRMING@users.noreply.github.com>
2023-11-23 23:53:27 +0800

    daily基本完善

---
+++ prcoil_eu_org_WebAPI/Models/Data/UserLoginData.cs
@@ -0,0 +1,12 @@
+﻿namespace prcoil_eu_org_WebAPI.Models.Data
+{
+    public class UserLoginData
+    {
+        //public string username { get; set; }
+        public string? Cellphone { get; set; }
+        public string? Password { get; set; }
+
+        public string? Captcha { get; set; }
+        public string? Remember { get; set; }
+    }
+}
---
+++ prcoil_eu_org_WebAPI/Models/Data/UserRegisterData.cs
@@ -0,0 +1,15 @@
+﻿namespace prcoil_eu_org_WebAPI.Models.Data
+{
+    public class UserRegisterData
+    {
+        public string? Username { get; set; }
+        public string? Cellphone { get; set; }
+        public string? Password { get; set; }
+
+
+
+        public string? Vercode { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public string? Agreement { get; set; }
+    }
+}
